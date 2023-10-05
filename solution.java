0603bcce0f103c9f597b151711b72ba64d79da16
Kooreis <kooreis.official@gmail.com>
2023-10-05 10:41:14 -0500

    Added a call to the 'groupAnagrams' function within the main method, passing the array of strings as an argument. The result is printed to the console.

--- solution.java
+++ solution.java
@@ -3,5 +3,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         String[] arr = {"cat", "dog", "tac", "god", "good", "act"};
+        System.out.println(groupAnagrams(arr));
     }
 }
