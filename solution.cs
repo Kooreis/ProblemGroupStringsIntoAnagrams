f38be9800af38694efd8213aa7426edf2625aad2
Kooreis <kooreis.official@gmail.com>
2023-10-05 10:41:14 -0500

    Added the main function. This function creates an array of words and calls the GroupAnagrams function. It then prints the grouped anagrams.

--- solution.cs
+++ solution.cs
@@ -1,7 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class Program
+static void Main(string[] args)
 {
+    string[] words = { "cat", "dog", "tac", "god", "good", "act" };
+    var groupedAnagrams = GroupAnagrams(words);
+
+    foreach (var group in groupedAnagrams)
+    {
+        Console.WriteLine(string.Join(", ", group));
+    }
 }
