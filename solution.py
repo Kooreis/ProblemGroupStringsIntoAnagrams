972b99d78d43a9fc52337f8eca86a2df121d3f95
Kooreis <kooreis.official@gmail.com>
2023-10-05 10:41:14 -0500

    Defined a function to group anagrams. This function takes a list of strings, sorts each string, and uses the sorted string as a key in a dictionary. The original string is then appended to the list of values for that key. The function returns a list of the values in the dictionary, which are the groups of anagrams.

--- solution.py
+++ solution.py
@@ -1,1 +1,5 @@
-from collections import defaultdict
+def group_anagrams(strs):
+    anagrams = defaultdict(list)
+    for word in strs:
+        anagrams["".join(sorted(word))].append(word)
+    return list(anagrams.values())
